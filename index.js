321112f1c4fd57b193b77ad7f425f4e0625a24b9
BHARTESH KAUSHIK <60025263+dirtylegs@users.noreply.github.com>
2021-09-06 23:55:15 +0530

    db

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const { nextTick } = require('process');
 const port = 8000;
 
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,28 +26,37 @@ app.use(express.static('assets')); //static
 //     next();
 // })
 
-var contactList = [
-    {
-        name: "bhartesh",
-        phone: "1234567890"
-    },
-    {
-        name: "light yagami",
-        phone: "1289687580"
-    },
-    {
-        name: "L",
-        phone: "1363986183"
-    }
-]
+// var contactList = [
+//     {
+//         name: "bhartesh",
+//         phone: "1234567890"
+//     },
+//     {
+//         name: "light yagami",
+//         phone: "1289687580"
+//     },
+//     {
+//         name: "L",
+//         phone: "1363986183"
+//     }
+// ]
 
 
 app.get('/', function(req,res){ //controller
     // console.log('from the get route controller', req.myName);
-    return res.render('home', {
-        title: "Contacts List",
-        contacts_list: contactList
+    
+    Contact.find({}, function(err, contacts){
+        if(err) {
+            console.log("Error in fetching contacts in db");
+            return;
+        }
+
+        return res.render('home', {
+            title: "Contacts List",
+            contacts_list: contacts
+        });
     });
+
 });
 
 app.get('/practice', function(req,res) {
@@ -56,24 +68,44 @@ app.get('/practice', function(req,res) {
 app.post('/create-contact', function(req,res) {
     // return res.redirect('/practice');
 
-    contactList.push({
+    // contactList.push({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // });
+
+    Contact.create({
         name: req.body.name,
         phone: req.body.phone
-    });
+    }, function(err, newContact){
+        if(err){console.log('Error in creating a contact!')
+            return;}
+            console.log('******', newContact);
+            return res.redirect('back');
+    })
 
-    return res.redirect('/');
+    // return res.redirect('/');
 });
 
 app.get('/delete-contact', function(req,res) {
-    let contact = req.query.phone;
+    // get id fromm query in the ul
+    let id = req.query.id;
+    // let phone = req.query.phone;
 
-    let contactIndex = contactList.findIndex(val => val.phone == contact);
+    // find the contact in the db using id and delete
+   Contact.findByIdAndDelete(id, function(err){
+        if(err) {
+            console.log("error in deleting an object from the database");
+            return;
+        }
 
-    if(contactIndex != -1) {
-        contactList.splice(contactIndex,1);
-    }
+        return res.redirect('back');
+   });
+    //let contactIndex = contactList.findIndex(contact => contact.phone == phone);
 
-    return res.redirect('back');
+    // if(contactIndex != -1) {
+    //     contactList.splice(contactIndex,1);
+    // }
+
 });
 
 app.listen(port, function(err) {
